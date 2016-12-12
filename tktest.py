ea44e064214f7a16a235385c9ed162c1f7dd0b37
guyongfei <guyongfei@redstone.net.cn>
2016-12-12 09:40:10 +0800

    use grid to arrange widgets

--- tktest.py
+++ tktest.py
@@ -42,6 +42,10 @@ def askopendestfilename():
 def doquit():
 	exit()
 
+def domakecmd():
+	cmdstr = makecmdstr('./makecmd')
+	print cmdstr
+
 def doit():
 	srcfilename = srcstrvar.get()
 	destfilename = deststrvar.get()
@@ -93,64 +97,108 @@ def parsexml(xmlfile):
 	tovalue = tonode.childNodes[0].data
 	print tovalue
 
+EPAD = 3
+
 top = Tk()
-srcframe = Frame(top)
-srcframe.pack()
-destframe = Frame(top)
-destframe.pack()
-radioframe = Frame(top)
-radioframe.pack()
-checkframe = Frame(top)
-checkframe.pack()
-cmdframe = Frame(top)
-cmdframe.pack()
-btnframe = Frame(top)
-btnframe.pack()
-
-srclabel = Label(srcframe, text='src: ')
-srclabel.pack(side=LEFT)
+srclabel = Label(top, text='src: ')
 srcstrvar = StringVar()
-srcentry = Entry(srcframe, bd=5, textvariable=srcstrvar)
-srcentry.pack(side=LEFT)
-srcbtn = Button(srcframe, text='select', command = askopensrcfilename)
-srcbtn.pack(side=RIGHT)
+srcentry = Entry(top, bd=5, textvariable=srcstrvar)
+srcbtn = Button(top, text='select', command = askopensrcfilename)
+top.rowconfigure(0,weight=1)
+top.columnconfigure(1,weight=1)
+srclabel.grid(column=0,row=0, padx=EPAD)
+srcentry.grid(column=1,columnspan=15,row=0, sticky=E+W, padx=EPAD)
+srcbtn.grid(column=16, row =0, padx=EPAD)
 
-
-destlabel = Label(destframe, text='dest:')
-destlabel.pack(side=LEFT)
+destlabel = Label(top, text='dest:')
 deststrvar = StringVar()
-destentry = Entry(destframe, bd=5, textvariable=deststrvar)
-destentry.pack(side=LEFT)
-destbtn = Button(destframe, text='select', command = askopendestfilename)
-destbtn.pack(side=RIGHT)
+destentry = Entry(top, bd=5, textvariable=deststrvar)
+destbtn = Button(top, text='select', command = askopendestfilename)
+destlabel.grid(column=0, row=1, padx=EPAD)
+destentry.grid(column=1,columnspan=15,row=1, sticky=E+W, padx=EPAD)
+destbtn.grid(column=16, row =1, padx=EPAD)
 
 radiovar = IntVar()
-rqualcom = Radiobutton(radioframe, text="Qualcom", variable=radiovar, value=1, command=sel)
-rqualcom.pack(side=LEFT)
-rmtkcom = Radiobutton(radioframe, text="Mtk", variable=radiovar, value=2, command=sel)
-rmtkcom.pack(side=LEFT)
-rothercom = Radiobutton(radioframe, text="Other", variable=radiovar, value=3, command=sel)
-rothercom.pack(side=RIGHT)
+rqualcom = Radiobutton(top, text="Qualcom", variable=radiovar, value=1, command=sel)
+rmtkcom = Radiobutton(top, text="Mtk", variable=radiovar, value=2, command=sel)
+rothercom = Radiobutton(top, text="Other", variable=radiovar, value=3, command=sel)
 
 chkvar1 = IntVar()
 chkvar2 = IntVar()
-chkbtn1 = Checkbutton(checkframe, text='checkbox1', variable=chkvar1, onvalue=1, \
+chkbtn1 = Checkbutton(top, text='checkbox1', variable=chkvar1, onvalue=1, \
 	offvalue=0, command=chkbtn1_sel)
-chkbtn1.pack()
-chkbtn2 = Checkbutton(checkframe, text='checkbox2', variable=chkvar2, onvalue=1, \
+chkbtn2 = Checkbutton(top, text='checkbox2', variable=chkvar2, onvalue=1, \
 	offvalue=0, command=chkbtn2_sel)
-chkbtn2.pack()
 
-cmdlabel = Label(cmdframe, text='cmd:')
-cmdlabel.pack(side=LEFT)
+cmdlabel = Label(top, text='cmd:')
 cmdstrvar = StringVar()
-cmdentry = Entry(cmdframe, bd=5, textvariable= cmdstrvar)
-cmdentry.pack(side=RIGHT)
+cmdentry = Entry(top, bd=5, textvariable= cmdstrvar)
 
 
-btn1 = Button(btnframe, text='cancel', command=doquit)
-btn1.pack(side=LEFT)
-btn2 = Button(btnframe, text='ok', command=doit)
-btn2.pack(side=RIGHT)
+btncancel = Button(top, text='cancel', command=doquit)
+btnmakecmd = Button(top, text='makeCmd', command=domakecmd)
+btnok = Button(top, text='ok', command=doit)
+
+
+
+# srcframe = Frame(top)
+# srcframe.pack()
+# destframe = Frame(top)
+# destframe.pack()
+# radioframe = Frame(top)
+# radioframe.pack()
+# checkframe = Frame(top)
+# checkframe.pack()
+# cmdframe = Frame(top)
+# cmdframe.pack()
+# btnframe = Frame(top)
+# btnframe.pack()
+
+# srclabel = Label(srcframe, text='src: ')
+# srclabel.pack(side=LEFT)
+# srcstrvar = StringVar()
+# srcentry = Entry(srcframe, bd=5, textvariable=srcstrvar)
+# srcentry.pack(side=LEFT)
+# srcbtn = Button(srcframe, text='select', command = askopensrcfilename)
+# srcbtn.pack(side=RIGHT)
+
+
+# destlabel = Label(destframe, text='dest:')
+# destlabel.pack(side=LEFT)
+# deststrvar = StringVar()
+# destentry = Entry(destframe, bd=5, textvariable=deststrvar)
+# destentry.pack(side=LEFT)
+# destbtn = Button(destframe, text='select', command = askopendestfilename)
+# destbtn.pack(side=RIGHT)
+
+# radiovar = IntVar()
+# rqualcom = Radiobutton(radioframe, text="Qualcom", variable=radiovar, value=1, command=sel)
+# rqualcom.pack(side=LEFT)
+# rmtkcom = Radiobutton(radioframe, text="Mtk", variable=radiovar, value=2, command=sel)
+# rmtkcom.pack(side=LEFT)
+# rothercom = Radiobutton(radioframe, text="Other", variable=radiovar, value=3, command=sel)
+# rothercom.pack(side=RIGHT)
+
+# chkvar1 = IntVar()
+# chkvar2 = IntVar()
+# chkbtn1 = Checkbutton(checkframe, text='checkbox1', variable=chkvar1, onvalue=1, \
+# 	offvalue=0, command=chkbtn1_sel)
+# chkbtn1.pack()
+# chkbtn2 = Checkbutton(checkframe, text='checkbox2', variable=chkvar2, onvalue=1, \
+# 	offvalue=0, command=chkbtn2_sel)
+# chkbtn2.pack()
+
+# cmdlabel = Label(cmdframe, text='cmd:')
+# cmdlabel.pack(side=LEFT)
+# cmdstrvar = StringVar()
+# cmdentry = Entry(cmdframe, bd=5, textvariable= cmdstrvar)
+# cmdentry.pack(side=RIGHT)
+
+
+# btn1 = Button(btnframe, text='cancel', command=doquit)
+# btn1.pack(side=LEFT)
+# btn2 = Button(btnframe, text='ok', command=doit)
+# btn2.pack(side=RIGHT)
+top.geometry('500x80+20+20')
 
 top.mainloop()
